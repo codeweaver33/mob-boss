48158670c28e249eff9cfdc8b6d6772f5a35935c
Dillon Bogenreif <codeweaver33@users.noreply.github.com>
2016-02-22 14:37:46 -0600

    Update reporter.py

--- reporter.py
+++ reporter.py
@@ -1,3 +1,18 @@
+#    mob-boss is a suricata rule management program
+#    Copyright (C) 2016  Dillon Bogenreif, Luke Young, Brandon Lattin
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # Imports
 import os
 import re
